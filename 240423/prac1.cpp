0f0c9f6ab6335d95febd8e88df1f143c8dbbb4eb
YejeongKang <dpwjd1130@gmail.com>
2024-04-23 16:17:42 +0900

    Add project files.

---
+++ 240423/prac1.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    for (int i = 0; i < 5; i++) 
+    {
+        std::cout << "Iteration : " << i + 1 << std::endl;
+    }
+    for (int j = 0; j < 10000; j++) 
+    {
+        std::cout << j << std::endl;
+    }
+
+    return 0;
+}
