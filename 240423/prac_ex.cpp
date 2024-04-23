68b4a29c116e6a25398ee3c1e1588abcdd323d03
YejeongKang <dpwjd1130@gmail.com>
2024-04-23 20:17:35 +0900

    pracdone_fix

--- 240423/prac_ex.cpp
+++ 240423/prac_ex.cpp
@@ -6,19 +6,28 @@ int main() {
 
 	int rep;
 	int j = 0;
+	
 	cout << "Input : ";
 	cin >> rep;
 	cout << endl << "Output : " << endl;
+	
+	string star = "";
 
 	for (int i = 1; i <= rep; i++) {
 
+		/*
+		1)
 		while (j != i) {
 			cout << "*";
 			j++;
 		}
 
 		j = 0;
-		cout << endl;
+		cout << endl;*/
+		
+		// 2)
+		star += "*";
+		cout << star << endl;
 	}
 	return 0;
 }
