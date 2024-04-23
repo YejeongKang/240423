5fc2014057b2ce4b5d7048fd1afcde02b29230b1
YejeongKang <dpwjd1130@gmail.com>
2024-04-23 17:13:38 +0900

    goo goo

--- 240423/prac1.cpp
+++ 240423/prac1.cpp
@@ -4,19 +4,19 @@ int main() {
 
 	std::cout << "(1) 5단 출력\n----5단----\n";
 
-	for (int i = 0; i < 9; i++) {
-		std::cout << "5 * " << i + 1 << " = " << 5 * (i + 1) << std::endl;
+	for (int i = 1; i < 10; i++) {
+		std::cout << "5 * " << i << " = " << 5 * i  << std::endl;
 
 	}
 
-	std::cout << "(2) 1~9단 출력\n";
+	std::cout << "(2) 1 ~ 9단 출력\n";
 
-	for (int j = 0; j < 9; j++) {
-		std::cout << "----" << j + 1 << "단----\n";
+	for (int j = 1; j < 10; j++) {
+		std::cout << "----" << j << "단----\n";
 		
-		for (int k = 0; k < 9; k++) {
+		for (int k = 1; k < 10; k++) {
 
-		std::cout << j + 1 << " * " << k + 1 << " = " << (j + 1) * (k + 1) << std::endl;
+		std::cout << j << " * " << k << " = " << j * k << std::endl;
 
 		}
 
